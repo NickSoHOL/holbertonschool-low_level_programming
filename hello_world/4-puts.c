0bf06c15655238267bdb16c5152c934e579538c8
root <nicolasrodriguezbustos@gmail.com>
2023-02-18 17:49:48 -0800

    low

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,1 +1,23 @@
-echo '#include <stdio.h>' > prog.c && echo 'int main(){puts("Programming is like building a multilingual puzzle,");return 0;}' >> prog.c && gcc prog.c -o prog && ./prog
+/**
+
+ * main - Prints "\"Programming is like building a multilingual puzzle"
+
+ * Return: 0
+
+ */
+
+
+
+#include <stdio.h>
+
+
+
+int main(void)
+  
+{
+  
+  puts("\"Programming is like building a multilingual puzzle");
+  
+  return (0);
+  
+}
