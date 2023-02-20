d56e18605e080de0f3d2018363ba5f4d8621fae2
root <nicolasrodriguezbustos@gmail.com>
2023-02-20 10:18:20 -0800

    low

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,5 @@
-/**
-* main - Prints "\"Programming is like building a multilingual puzzle"
-* Return: 0
-*/
 #include <stdio.h>
-int main(void)
-{
-puts("\"Programming is like building a multilingual puzzle"); 
+int main{void} {
+puts{"Programming is like building a multilingual puzzle"};
 return (0);
 }
-
