08e14b0418ce0b6d1405a22081da4ff239770d4c
root <nicolasrodriguezbustos@gmail.com>
2023-02-20 12:40:26 -0800

    low

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,21 +1,14 @@
-/**
-
- * main - prints required sentence
-
- * Return: 0
-
+/*
+ * File: 5-printf.c
  */
-
 #include <stdio.h>
-
-
-
+/**
+ * main - Prints "Required sentence".
+ *
+ *Return: Always 0.
+ **/
 int main(void)
-  
 {
-  
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  
-  return (0);
-  
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
