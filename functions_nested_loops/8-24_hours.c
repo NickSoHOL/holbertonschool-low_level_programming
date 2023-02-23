b0693ec15a2daaefa490a210a699ab4ea40c0113
root <nicolasrodriguezbustos@gmail.com>
2023-02-23 06:32:36 -0800

    functions

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -6,7 +6,8 @@
  * jack_bauer - Prints every minute of the day of
  *              Jack Bauer, starting from 00:00 to 23:59.
  */
-void jack_bauer(void) 
+void jack_bauer(void)
+{
 int a, b, c, d;
 for (a = 48; a <= 50; a++)
 {
