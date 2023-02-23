88121038029142eeccb54b9543c69129a510bea6
root <nicolasrodriguezbustos@gmail.com>
2023-02-23 07:01:02 -0800

    functions

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,20 +9,16 @@
  **/
 void print_to_98(int n)
 {
-int i;
-if (n <= 98)
+if (n >= 98)
 {
-for (i = n; i < 98; i++)
-{
-printf("%d, ", i);
-}
+for (; n > 98; n--)
+printf("%d, ", n);
+printf("%d\n", n);
 }
 else
 {
-for (i = n; i > 98; i--)
-{
-printf("%d, ", i);
-}
-}
-printf("98\n"); 
+for (; n < 98; n++)
+printf("%d, ", n);
+printf("%d\n", n);
+} 
 }
