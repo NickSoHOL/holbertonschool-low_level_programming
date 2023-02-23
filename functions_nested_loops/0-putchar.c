4d2842394b08ff9d7154f7c62d50b308cb36f691
root <nicolasrodriguezbustos@gmail.com>
2023-02-22 17:11:19 -0800

    functions

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+/*
+ * File: 0-putchar.c
+ */
+#include "_putchar"
+/**
+ * main - Prints "_putchar" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return 0;
+}
