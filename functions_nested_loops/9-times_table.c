143df997109e2c5758a0d3742002d9105c3ce308
root <nicolasrodriguezbustos@gmail.com>
2023-02-23 06:40:56 -0800

    functions

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -6,10 +6,32 @@
  * times_table - Prints the 9 times table, starting with 0.
  **/
 void times_table(void)  
+int a, b, c;
+for (a = 0; a <= 9; a++)
 {
-int num;
-for (num = 0; num <= 10; num++)
+for (b = 0; b <= 9; b++)
 {
-printf("9 x %d = %d\n", num, 9 * num);
-}  
+c = a * b;
+if (b == 0)
+{
+_putchar(c + '0');
+}
+else if (c <= 9)
+{
+_putchar(32);
+_putchar(c + '0');
+}
+else
+{
+_putchar((c / 10) + '0');
+_putchar((c % 10) + '0');
+}
+if (b != 9);
+{
+_putchar(',');
+_putchar(32);
+}
+}
+_putchar('\n');
+}
 }
