4389297a14d408abf50bc0588c5bff3776d8b937
root <nicolasrodriguezbustos@gmail.com>
2023-02-22 16:15:46 -0800

    low

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -8,7 +8,7 @@
  *
  * Return: Always 0.
  */
-int main(void) 
+int main(void)
 {
 int i;
 for (i = 0; i < 10; i++)
@@ -16,5 +16,5 @@ for (i = 0; i < 10; i++)
 putchar(i + '0');
 putchar('\n');
 }
-return 0;  
+return 0;
 }
