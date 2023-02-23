dd2153add72dcc86212eab914db002858e69404c
root <nicolasrodriguezbustos@gmail.com>
2023-02-22 16:22:37 -0800

    low

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -10,11 +10,9 @@
  */
 int main(void)
 {
-int i;
-for (i = 0; i < 10; i++)
-{
-putchar((i % 10) + '0');
+int num;
+for (num = 0; num < 10; num++)
+putchar((num % 10) + '0');
 putchar('\n');
-}
-return (0);
+return (0); 
 }
