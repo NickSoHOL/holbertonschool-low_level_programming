84d38469b07290f7451c1781ec353652603dc05f
root <nicolasrodriguezbustos@gmail.com>
2023-02-23 06:24:23 -0800

    functions

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -9,7 +9,9 @@
  **/
 int print_last_digit(int n)
 {
-int last_digit = abs(num) % 10;
-printf("Last digit of %d is %d\n", num, last_digit);
-return last_digit;
+int last_digit = n % 10;
+if (last_digit < 0)
+last_digit *= -1;
+_putchar(last_digit + '0');
+return (last_digit);
 }
