de2aed8361f841fee87de5b8effbc2b6e778adc4
root <nicolasrodriguezbustos@gmail.com>
2023-02-22 17:31:20 -0800

    functions

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -5,7 +5,7 @@
 /**
  * main - Prints "the alphabet in lowercase, followed by a new line.
  */
-print_alphabet(void)
+int print_alphabet(void)
 {
 char letter;
 for (letter = 'a'; letter <= 'z'; letter++)   
