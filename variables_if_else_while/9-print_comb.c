1e347d217980a2589489ed2d2ca0056aef9ce303
root <nicolasrodriguezbustos@gmail.com>
2023-02-22 16:48:00 -0800

    low

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -10,21 +10,13 @@
 int main(void)
 {
 int num;
-for (num = 0; num < 10; num++)
+for (num = 0; num <= 9; num++)
 {
-int num2;
-for (num2 = num; num2 < 10; num2++)
-{
-putchar(num + '0');
+putchar((num % 10) + '0');
+if (num == 9)
+continue;
 putchar(',');
 putchar(' ');
-putchar(num2 + '0');
-if (num != 9 || num2 != 9)
-{
-putchar(',');
-putchar(' ');
-}
-}
 }
 putchar('\n');
 return (0);
