58f5f0138a2d873bbc495632f6a44444ce53ce10
root <nicolasrodriguezbustos@gmail.com>
2023-02-28 07:30:44 -0800

    files

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+/*
+ * File: 0-reset_to_98.c
+ */
+#include <stdio.h>
+/**
+ *main - pointer to an int as parameter and updates the value it points to to 98.
+ *
+ * Return: Always 0.
+ **/
+void reset_to_98(int *n);
